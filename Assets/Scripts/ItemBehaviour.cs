929ea0a7b0901f61109c772520965bfceccd4c34
Nicholas Arnaud <cobrahead09@gmail.com>
2017-05-11 18:19:34 -0500

    Resolve Conflicts

---
+++ Assets/Scripts/ItemBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehaviour : MonoBehaviour
+{
+    public Item itemConfig;
+    private Item _runtimeItem;
+
+
+    private void Start()
+    {
+        _runtimeItem = itemConfig;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "backpack")
+        {
+            other.GetComponent<BackpackBehaviour>().AddToPack(_runtimeItem);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour
+{
+    public List<GameObject> items;
+	// Use this for initialization
+	void Start ()
+    {
+		items.ForEach(x => Instantiate(x));
+    }
+}
