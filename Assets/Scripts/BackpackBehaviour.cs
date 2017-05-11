a9be59ea3c55741067a6a82119c40c9812c2feb5
Nicholas Arnaud <cobrahead09@gmail.com>
2017-05-11 17:55:08 -0500

    Added spawn and delete behaviours

---
+++ Assets/Scripts/BackpackBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackBehaviour : MonoBehaviour
+{
+    public Backpack BackpackConfig;
+    public List<Item> Invetory;
+    public int Capacity;
+    private void Start()
+    {
+        BackpackConfig = Instantiate(BackpackConfig);
+        Invetory = BackpackConfig.CurrentBackPack;
+    }
+
+    public void AddToPack(Item item)
+    {
+        Invetory.Add(item);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Invetory.RemoveAt(0);
+        }
+    }
+}
